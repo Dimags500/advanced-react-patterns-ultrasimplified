5a78e5db4811f7b4d6a188cc4ece007f30bb3e74
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-09-05 14:55:48 +0300

    Reusable class & inline styles

--- showcase/src/patterns/04.js
+++ showcase/src/patterns/04.js
@@ -1,6 +1,7 @@
 import React, { Component, useState, useEffect, useCallback } from "react";
 import mojs from "mo-js";
 import Styles from "./index.css";
+import userCustomStyles from "./usage.css";
 
 const initalState = {
   count: 0,
@@ -101,7 +102,12 @@ const useClapAnimation = ({ clapEl, countEl, clapTotalEl }) => {
 const MedumClapContex = React.createContext();
 const { Provider } = MedumClapContex;
 
-const MediumClap = ({ children, onClap, style: userStyles = {} }) => {
+const MediumClap = ({
+  children,
+  onClap,
+  style: userStyles = {},
+  className,
+}) => {
   const MAX_USER_CLAP = 15;
   const [clapState, setClapState] = useState(initalState);
   const { count, countTotal, isClicked } = clapState;
@@ -147,12 +153,14 @@ const MediumClap = ({ children, onClap, style: userStyles = {} }) => {
     [clapState, setRef]
   );
 
+  const classNames = [Styles.clap, className].join(" ").trim();
+
   return (
     <Provider value={memoizedValue}>
       <button
         ref={setRef}
         data-refkey="clapRef"
-        className={Styles.clap}
+        className={classNames}
         onClick={hendlerClapClick}
         style={userStyles}
       >
@@ -163,14 +171,18 @@ const MediumClap = ({ children, onClap, style: userStyles = {} }) => {
 };
 
 //sub components
-const ClapIcon = ({ style: userStyles = {} }) => {
+const ClapIcon = ({ style: userStyles = {}, className }) => {
   const { isClicked } = React.useContext(MedumClapContex);
+
+  const classNames = [Styles.icon, isClicked ? Styles.checked : "", className]
+    .join(" ")
+    .trim();
   return (
     <span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-549 338 100.1 125"
-        className={`${Styles.icon} ${isClicked && Styles.checked} `}
+        className={classNames} //`${Styles.icon} ${isClicked && Styles.checked} `}
         style={userStyles}
       >
         <path d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z" />
@@ -180,14 +192,15 @@ const ClapIcon = ({ style: userStyles = {} }) => {
   );
 };
 
-const ClapCount = ({ style: userStyles = {} }) => {
+const ClapCount = ({ style: userStyles = {}, className }) => {
   const { count, setRef } = React.useContext(MedumClapContex);
 
+  const classNames = [Styles.count, className].join(" ").trim();
   return (
     <span
       ref={setRef}
       data-refkey="clapCountRef"
-      className={Styles.count}
+      className={classNames}
       style={userStyles}
     >
       +{count}
@@ -195,14 +208,16 @@ const ClapCount = ({ style: userStyles = {} }) => {
   );
 };
 
-const CountTotal = ({ style: userStyles = {} }) => {
+const CountTotal = ({ style: userStyles = {}, className }) => {
   const { countTotal, setRef } = React.useContext(MedumClapContex);
+
+  const classNames = [Styles.total, className].join(" ").trim();
 
   return (
     <span
       ref={setRef}
       data-refkey="clapCountTotalRef"
-      className={Styles.total}
+      className={classNames}
       style={userStyles}
     >
       {countTotal}
@@ -237,10 +252,17 @@ const Usage = () => {
   };
   return (
     <div style={{ width: "100%" }}>
-      <MediumClap onClap={handleClap} style={{ border: " 1px solid red" }}>
-        <MediumClap.Icon />
-        <MediumClap.Count style={{ color: "red" }} />
-        <MediumClap.Toatl />
+      <MediumClap
+        onClap={handleClap}
+        style={{ border: " 1px solid red" }}
+        className={userCustomStyles.clap}
+      >
+        <MediumClap.Icon className={userCustomStyles.icon} />
+        <MediumClap.Count
+          style={{ color: "red" }}
+          className={userCustomStyles.count}
+        />
+        <MediumClap.Toatl className={userCustomStyles.total} />
       </MediumClap>
       <div className={Styles.info}>{`total clicks ${count}`}</div>
     </div>
